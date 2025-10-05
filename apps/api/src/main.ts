ed473df240276ae8762c1404da107496eb89406e
naz-projects <nazhossain16@gmail.com>
2025-10-05 15:44:23 -0400

    enabled cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: process.env.FRONTEND_URL || 'http://localhost:3001', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true});
+  
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || undefined;
   await app.listen(port, host);
